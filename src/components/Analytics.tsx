071ed79de46e2ddc35812f82aee22a6f285fed00
Kaypee-43 <128208727+Kaypee-43@users.noreply.github.com>
2026-08-06 15:55:10 +0530

    Update Analytics.tsx

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -9,6 +9,15 @@ const PIXEL = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 export default function Analytics() {
   return (
     <>
+      return (
+    <>
+      <Script id="consent-default" strategy="beforeInteractive">
+        {`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
+gtag('consent','default',{ad_storage:'denied',ad_user_data:'denied',ad_personalization:'denied',analytics_storage:'denied',wait_for_update:500,region:['GB','AT','BE','BG','HR','CY','CZ','DK','EE','FI','FR','DE','GR','HU','IE','IT','LV','LT','LU','MT','NL','PL','PT','RO','SK','SI','ES','SE','IS','LI','NO']});
+gtag('consent','default',{ad_storage:'granted',ad_user_data:'granted',ad_personalization:'granted',analytics_storage:'granted'});
+gtag('set','url_passthrough',true);`}
+      </Script>
+      {GTM && (
       {GTM && (
         <Script id="gtm" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM}');`}
